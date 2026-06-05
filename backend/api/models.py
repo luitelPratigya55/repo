3969f37e1c67c67e65a0dd7ea7e23a9e96b96425
luitelPratigya55 <157899609+luitelPratigya55@users.noreply.github.com>
2026-06-05 14:37:22 +0545

    created a url model, and serializer for short url creation

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class URL(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="user_url")
+    
+    long_url = models.URLField(max_length=1000)
+    short_code = models.CharField(max_length=20, unique=True, db_index=True)
+    
+    is_active = models.BooleanField(default=True)
+    clicks = models.IntegerField(default=0)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    
+    class Meta:
+        ordering = ['-created_at']  
+    
+    
+    def __str__(self):
+        return f'${self.user.username} has created ${self.long_url}' 
--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from .models import URL
 
 class RegistrationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,3 +14,42 @@ class RegistrationSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
+class CreateURLSerializer(serializers.ModelSerializer):
+    
+    short_url = serializers.SerializerMethodField()
+    
+    class Meta:
+        model=URL
+        fields = ["id","user","long_url","short_code","short_url","created_at", "clicks", "is_active"]
+        extra_kwargs = {
+            "short_code":{
+                "read_only": True
+            },
+            
+            "short_url":{
+                "read_only": True
+            },
+            
+            "created_at":{
+                "read_only": True
+            },
+            
+            "clicks":{
+                "read_only": True
+            }
+            
+        }
+        
+        def get_short_url(self,obj):
+            pass
+        
+        def generate_short_code(self, long_url):
+            
+            pass
+        
+        def validate_url(self,value):
+            pass
+        
+        
+        def create(self,validated_data):
+            pass
